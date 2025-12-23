168c9dc577098a9aef4e26dc44ea14d85aa3e972
eetee23 <eetu.lep02@gmail.com>
2025-12-23 15:13:23 +0200

    added new function for enum and struct and send mutable hashmap to add_employee function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod user_choice;
 mod employee;
 
+#[derive(Debug)]
 pub enum Departments {
     Business,
     HR,
@@ -9,12 +10,35 @@ pub enum Departments {
     Management,
     Installation,
     Warehouse,
+    NoDepartment,
 }
 
+impl Departments {
+    pub fn new(value: u8) -> Departments {
+        match value {
+            1 => Departments::Business,
+            2 => Departments::HR,
+            3 => Departments::Development,
+            4 => Departments::Support,
+            5 => Departments::Management,
+            6 => Departments::Installation,
+            7 => Departments::Warehouse,
+            _ => Departments::NoDepartment,
+        }
+    }   
+}
+
+#[derive(Debug)]
 pub struct Employee {
     name: String,
     age: u8,
     department: Departments
+}
+
+impl Employee {
+    pub fn new(name: String, age: u8, department: Departments) -> Employee {
+        Employee { name, age, department }
+    }   
 }
 
 fn main() {
@@ -25,7 +49,6 @@ fn main() {
     println!("Employee system");
     let mut employees: HashMap<u32, Employee> = HashMap::new();
     loop {
-     
         println!("Enter choice for what you would like to do");
         println!("1. Enter employee");
         println!("2. Get all employees");
@@ -41,7 +64,7 @@ fn main() {
         match result {
             Choice::Exit => break,
             Choice::GetEmployees => get_employees(),
-            Choice::AddEmployees => add_employee(&employees),
+            Choice::AddEmployees => add_employee(&mut employees),
             Choice::InValidInput => {
                 println!("Invalid input!");
                 continue;
